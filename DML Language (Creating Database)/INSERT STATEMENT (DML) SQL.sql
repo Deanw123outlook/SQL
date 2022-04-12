4b80db81c12d0f891597b3c8bc8bb2d1cc0f7a44
Dean Watters <79112001+Deanw123outlook@users.noreply.github.com>
2022-04-12 13:31:26 +0200

    Add files via upload

---
+++ DML Language (Creating Database)/INSERT STATEMENT (DML) SQL.sql
@@ -0,0 +1,51 @@
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_C','Model_B','1990','308000','120','Petrol','Dublin','Ireland','250.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_A','Model_A','2022','5000','700','Diesel','Dublin','Ireland','44000.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_A','Model_B','2021','25000','650','Diesel','London','England','34250.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_C','Model_A','1989','450000','100','Petrol','London','England','250.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_A','Model_C','2014','55000','400','Diesel','London','England','4250.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_B','Model_A','2016','48000','400','Diesel','London','England','6250.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_A','Model_A','2018','45000','600','Diesel','London','England','13250.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_B','Model_B','2019','54000','600','Diesel','London','England','24250.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_A','Model_A','2019','45000','600','Diesel','London','England','20250.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_B','Model_A','2018','55000','600','Diesel','London','England','13250.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_B','Model_A','2018','75000','600','Diesel','London','England','12150.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_B','Model_B','2022','15000','700','Diesel','London','England','65250.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_B','Model_A','2018','35000','600','Diesel','London','England','18250.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_A','Model_A','1999','408000','250','Diesel','Dublin','Ireland','750.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_C','Model_B','2020','8000','500','Diesel','Dublin','Ireland','54000.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+values ('Make_B','Model_A','2012','95000','600','Diesel','London','England','7250.00')
+INSERT INTO dbo.Data
+(Make,Model,Year,Mileage,Vol_Engine,Fuel,City,Country,Price)
+SELECT * FROM dbo.Data
