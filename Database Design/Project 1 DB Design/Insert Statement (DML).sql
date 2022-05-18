f265e4157d30a46968bc12f7dc8bad517215575a
Dean Watters <79112001+Deanw123outlook@users.noreply.github.com>
2022-05-18 21:47:54 +0200

    Add files via upload

---
+++ Database Design/Project 1 DB Design/Insert Statement (DML).sql
@@ -0,0 +1,3 @@
+INSERT INTO dbo.Table_1
+(CaseType,CaseID,Date,CaseStartTime,CaseEndTime,CaseDescription,CaseStatis)
+values ('Case_A','1234554321','2016-12-21','2016-12-21 00:00:00.000','2016-12-21 00:00:00.000','Marketing Case Type Unknown','Complete');
